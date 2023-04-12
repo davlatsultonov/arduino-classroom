8625cfa7937bab1675f1cdb3380e02f50d5772f2
davlatsultonov <davlatsultonov2001@gmail.com>
2023-04-12 09:47:05 +0500

    add footer block (add unresolved files)

---
+++ resources/js/Shared/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="footer">
+        <div class="container">
+            <div class="row py-4 align-items-center">
+                <div class="col-4">
+                    <Logo dark-theme />
+                </div>
+                <div class="col-8">
+                    <p>
+                        О НАС
+                    </p>
+                    <p>
+                        ArduinoClassroom.tj - сайт для тех, кто использует в своих DIY проектах микроконтроллеры и ардуино. Перепечатка материалов возможна только с согласия авторов.
+                    </p>
+                    <p class="mb-0">
+                        Свяжитесь с нами: info@arduinoclassroom.tj
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-copyright text-center text-white py-3">
+            <div class="container">
+                © ArduinoClassroom.tj, 2023 -
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Logo from "./Logo.vue";
+
+export default {
+    name: "Footer",
+    components: {Logo}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Shared/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+    <Link class="text-decoration-none fs-4" :class="{ 'text-white': darkTheme }" href="/">Arduino<span class="d-block d-sm-inline"><b>CLASSROOM</b></span></Link>
+</template>
+
+<script>
+import {Link} from "@inertiajs/inertia-vue3";
+export default {
+    name: "Logo",
+    components: {Link},
+    props: {
+        darkTheme: Boolean,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
